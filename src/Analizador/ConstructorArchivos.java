b19296ac6b7a5e8a5df15df9d56213ee6e6361e6
Jhony Caro <jhonycaro1998@gmail.com>
2022-07-30 12:55:10 -0500

    Primer readme.md y sobreescritura de archivos de forma automatica al correr el proyecto

--- src/Analizador/ConstructorArchivos.java
+++ src/Analizador/ConstructorArchivos.java
@@ -8,31 +8,32 @@ import java.nio.file.Paths;
 
 /**
  *
- * @author Jhony Alejandro Caro
- * Junio 2022
+ * @author Jhony Alejandro Caro Junio 2022
  */
-public class AnalizadorLexico {
-    public static void main(String[] args) throws Exception {
-        String camino= "../AnalizadorLexico/src/Analizador/Lexico.flex";
-        String camino1= "../AnalizadorLexico/src/Analizador/LexicoCup.flex";
-        String[] caminoS= {"-parser","Sintaxis","../AnalizadorLexico/src/Analizador/Sintaxis.cup"};
-        generarLex(camino, camino1, caminoS);
-        
+public class ConstructorArchivos {
+
+    private String camino = "../AnalizadorLexico/src/Analizador/Lexico.flex";
+    private String camino1 = "../AnalizadorLexico/src/Analizador/LexicoCup.flex";
+    private String[] caminoS = {"-parser", "Sintaxis", "../AnalizadorLexico/src/Analizador/Sintaxis.cup"};
+
+    public ConstructorArchivos() throws Exception {
+        generarArchivos(camino, camino1, caminoS);
     }
-    public static void generarLex(String camino, String camino1, String[] caminoS) throws IOException, Exception {
-        File arc; 
+
+    private static void generarArchivos(String camino, String camino1, String[] caminoS) throws IOException, Exception {
+        File arc;
         arc = new File(camino);
         JFlex.Main.generate(arc);
         arc = new File(camino1);
         JFlex.Main.generate(arc);
-        java_cup.Main.main(caminoS);     
-        
+        java_cup.Main.main(caminoS);
+
         Path caminoSym = Paths.get("../AnalizadorLexico/src/Analizador/sym.java");
         if (Files.exists(caminoSym)) {
             Files.delete(caminoSym);
         }
         Files.move(
-                Paths.get("../AnalizadorLexico/sym.java"), 
+                Paths.get("../AnalizadorLexico/sym.java"),
                 Paths.get("../AnalizadorLexico/src/Analizador/sym.java")
         );
         Path caminoSint = Paths.get("../AnalizadorLexico/src/Analizador/Sintaxis.java");
@@ -40,8 +41,8 @@ public class AnalizadorLexico {
             Files.delete(caminoSint);
         }
         Files.move(
-                Paths.get("../AnalizadorLexico/Sintaxis.java"), 
+                Paths.get("../AnalizadorLexico/Sintaxis.java"),
                 Paths.get("../AnalizadorLexico/src/Analizador/Sintaxis.java")
-        );        
+        );
     }
 }
--- src/Analizador/FrmAnalizador.java
+++ src/Analizador/FrmAnalizador.java
@@ -457,15 +457,10 @@ public class FrmAnalizador extends javax.swing.JFrame {
         txtAnalizarLex.setText(null);
     }//GEN-LAST:event_Borrar1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+    public static void main(String args[]) throws Exception {
+        
+        new ConstructorArchivos(); 
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
